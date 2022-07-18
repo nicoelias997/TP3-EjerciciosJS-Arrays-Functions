eed5b10b69ffb2678241f420b4c788e32d180001
nicoelias997 <nicoelias997@gmail.com>
2022-07-18 20:18:21 +0200

    Termine de hacer el ejercicio 3, me gusto, estaba dificil, no se si es la manera mas optima de hacerlo, pero la salida es la esperada

--- js/arrays/ejercicio3.js
+++ js/arrays/ejercicio3.js
@@ -1,4 +1,4 @@
-// 3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+// // 3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
 
 let arraySuma = [];
 let arraySuma2 = [];
@@ -6,20 +6,21 @@ let arraySuma2 = [];
 do{
     let dado1 = Math.round(Math.random() * (6 - 1) + 1);
     let dado2 = Math.round(Math.random() * (6 - 1) + 1);
-    let suma = dado1 + dado2;
+    let suma = dado1 + dado2
     arraySuma.push(suma);
-    
 } while(arraySuma.length < 50);
 
-console.log(arraySuma);
-
-for(let i = 2; i < 12; i++){
-    arraySuma2.push(arraySuma.filter(numero => numero == i));
-}
-console.log(arraySuma2)
+document.write("El array de 50 veces la suma de dos dados es: " + arraySuma + "<br><br>");
 
 
-//no entiendo si me piden que anote en el array todas las sumas, o cuantas veces se repitio la misma suma
+for(let y = 2; y <= 12; y++){
+    arraySuma2.push(arraySuma.filter(n => n == y));
+    }
+    document.write("El array ordenado, donde nos muestra la cantidad de valores que hay de cada suma es: " + arraySuma2 + "<br><br>");
+    
 
-
+for(let x = 0; x <= 10; x++){ //tengo que hacer  que recorra 11 lugares, ya que son 11 los resultados posibles de la suma de los dados
+    document.write("La frecuencia con la que ocurre " + (x+2) + " es: ");
+    document.write(arraySuma2[x].length + "<br>") 
+}   
 
